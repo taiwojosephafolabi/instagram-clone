76ee16b6d1e80ad2141108f91d4e2a8c42e3d126
JTAfolabi <taiwojosephafolabi@gmail.com>
2023-12-05 01:18:18 +0000

    initialisation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
