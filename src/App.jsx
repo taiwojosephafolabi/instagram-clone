7721e6dbbbfd33243e1a71217a6e0655afde7d5c
JTAfolabi <taiwojosephafolabi@gmail.com>
2023-12-06 23:27:45 +0000

    added sidebar to page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import PageLayout from './Layouts/PageLayout/PageLayout';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import AuthPage from './pages/AuthPage/AuthPage';
 
+
 function App() {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />`
       </Routes>
-    </>
+    </PageLayout>
   );
 }
 
